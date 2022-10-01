d5c09a918ca67b0ecaf1ed0a3ce08659cbf15ff7
Sidharth Raj <sidharth-raj@outlook.com>
2022-10-02 01:37:02 +0530

    updated algo

--- Arrays and String/Longest Substring without Repeating Character/b.js
+++ Arrays and String/Longest Substring without Repeating Character/b.js
@@ -1,10 +1,17 @@
-// let array = "abcdefbea";
-let array = "GEEKSFORGEEKS";
+let array = "abcdefbea";
+// let array = "GEEKSFORGEEKS";
 
 let l = 0;
 let r = 0;
 let set = new Set();
 let max = 0;
+
+function updateMax() {
+    // update max
+    if (r - l + 1 > max) {
+        max = r - l + 1;
+    }
+}
 
 while (l >= 0 && r < array.length) {
     if (set.has(array[r])) {
@@ -12,20 +19,14 @@ while (l >= 0 && r < array.length) {
         set = new Set();
 
         console.log(array.substring(l, r));
-        // update max
-        if (r - l + 1 > max) {
-            max = r - l + 1;
-        }
+        updateMax();
 
         l = r;
-        r++;
     } else {
         set.add(array[r]);
 
-        // update max
-        if (r - l + 1 > max) {
-            max = r - l + 1;
-        }
+        updateMax();
+
         r++;
     }
 
