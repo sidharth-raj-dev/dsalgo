52f7bfe26efde36945f5e466d64cdf5192a4c99f
Sidharth Raj <sidharth-raj@outlook.com>
2022-10-02 18:07:25 +0530

    added trees algo

---
+++ Trees and Graphs/Binary Search Tree/a.js
@@ -0,0 +1,24 @@
+function Node(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+}
+
+
+class BinarySearchTree {
+    constructor() {
+        this.root = null;
+    }
+
+    push() {
+
+    }
+}
+
+
+const treeRef = new BinarySearchTree();
+treeRef.push(5);
+treeRef.push(1);
+treeRef.push(2);
+treeRef.push(3);
+treeRef.push(7);
